dbd353265601c7dbfa975ecccf5f535fcb53f4cf
harriswj <harrisw.14@gmail.com>
2017-04-04 14:18:39 -0400

    Add files via upload

---
+++ arr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* count digits, white spaces, others. */
+/* This is 1.6 Arrays in K and R */
+
+main() 
+{
+	int c, i, nwhite, nother;
+	int ndigit[10];
+
+	nwhite = nother = 0;
+	for ( i = 0; i < 10; ++i)
+		ndigit[i] = 0;
+	
+	while ((c = getchar()) != EOF)
+		if (c >= '0' && c <= '9')
+			++ndigit[c];
+		else if (c == ' ' || c == '\n' || c == '\t')
+			++nwhite;
+		else
+			++nother;
+
+	printf("digits =");
+	for (i = 0; i < 10; ++i)
+		printf(" %d", ndigit[i]);
+	printf(", white space = %d, other = %d\n", nwhite, nother);
+}
